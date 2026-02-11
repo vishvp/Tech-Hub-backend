d32711aa9ff389f78af9e85b60f9447140c7feb0
vishvp <pheonixvishv90@gmail.com>
2026-02-11 17:00:31 +0530

    hello

---
+++ src/Pages/Mangers/ManagersDashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ManagersDashboard() {
+    return (
+        <div>ManagersDashboard</div>
+    )
+}
---
+++ src/Pages/Mangers/MangersSidebar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MangersSidebar() {
+    return (
+        <div>MangersSidebar</div>
+    )
+}
